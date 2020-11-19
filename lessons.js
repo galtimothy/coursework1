7865006d1be84d3c2769dcf3f0ddd43c07cc87c3
Georgia <georgialouisatimothy@yahoo.com>
2020-11-19 10:54:20 +0000

    add lessons

---
+++ lessons.js
@@ -0,0 +1,42 @@
+let lessons = [
+    {
+        id: 1,
+        subject: 'English',
+        location: 'London',
+        image: 'image/english.jpg',
+        price: 2000,
+        availability: 5
+    },
+    {
+        id: 2,
+        subject: 'Mathamatic',
+        location: 'London',
+        image: 'image/mathamatics.jpg',
+        price: 2500,
+        availability: 5
+    },
+    {
+        id: 3,
+        subject: 'Music',
+        location: 'London',
+        image: 'image/music.jpg',
+        price: 3500,
+        availability: 5
+    },
+    {
+        id: 4,
+        subject: 'Art',
+        location: 'London',
+        image: 'image/art.jpg',
+        price: 1500,
+        availability: 5
+    },
+    {
+        id: 5,
+        subject: 'Drama',
+        location: 'London',
+        image: 'image/drama.jpg',
+        price: 2500,
+        availability: 5
+    }
+]
